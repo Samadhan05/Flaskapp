61397c5a139b6fe242bc3a2ed64ec2e2609e164d
Samadhan55 <Samadhan55@github.com>
2021-12-22 12:06:10 +0545

    database table Todo created

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+class Todo(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(100))
+    content=db.Column(db.String(300))
+    iscomplete=db.Column(db.Boolean)
 
 @app.route('/')
 def home():
@@ -8,6 +16,6 @@ def home():
 
 @app.route('/tasks')
 def tasks():
-	return render_template('tasks.html')
+	return render_template('task.html')
 if __name__ == '__main__':
     app.run(debug=True)
